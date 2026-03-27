eb62f298e2eb8155c156bec805ac29faf02241ae
aali006 <ashmat.ali0777@gmail.com>
2026-03-27 14:45:01 +0530

    new_commit15

--- backend/services/rag_service.py
+++ backend/services/rag_service.py
@@ -56,7 +56,7 @@ def get_hr_chat_response(user_query: str, stream: bool = False):
         response = requests.post(
             "http://localhost:11434/api/generate",
             json={
-                "model": "llama3.2:8b", # Ensure this model is pulled in Colab!
+                "model": "llama3.1:8b", # Ensure this model is pulled in Colab!
                 "prompt": prompt,
                 "stream": stream
             },
